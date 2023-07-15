25ddd0340a53752ce9ce2999ee58cf4aed3a023a
faketri <mrandrey1231@mail.ru>
2023-07-15 17:57:45 +0300

    delete unnecessary imports

--- backend/Cinema/src/main/java/com/Online/Cinema/security/SecurityConfigurator.java
+++ backend/Cinema/src/main/java/com/Online/Cinema/security/SecurityConfigurator.java
@@ -1,17 +1,12 @@
 package com.Online.Cinema.security;
 
 import com.Online.Cinema.service.UserDetailsServiceImpl;
-import com.Online.Cinema.service.UserService;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -50,10 +45,4 @@ public class SecurityConfigurator {
     public UserDetailsService CustomUserDetailsService(){
         return userDetailsServiceImpl;
     }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
 }
